c3d27cbb4f427cd13e25e4f262fdabf3b563f7a7
Rodrigo Santiago Gomide <rodrigogomide.def@gmail.com>
2022-08-11 14:38:09 -0300

    R7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,9 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('', () => {
+    getSavedCartItems()
+    expect(localStorage.getItem).toBeCalled()
+    expect(localStorage.getItem).toBeCalledWith('cartItems')
+  });
 });
